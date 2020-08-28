a481085cfa5b4c666bc816a0bccbdedadb9f9d2e
margare-jihua <margaret.jihua@gmail.com>
2020-08-28 10:52:40 -0700

    [Home] Add Home Component

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Mars Dentistry</h1>
+                <p>We the dentists go for their dental needs.</p>
+            </div>
+        )
+    }
+}
+
+export default Home
