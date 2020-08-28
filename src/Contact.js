b584059b399dfb6f9ffbb2d3ad3d7008a0ae1d44
margare-jihua <margaret.jihua@gmail.com>
2020-08-28 11:00:45 -0700

    [Procedures][Contact] Add Component

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contact Us</h1>
+                <p>Margaret Lee, MD</p>
+                <p>Email: margaret.jihua@gmail.com</p>
+                <p>Phone: 111-222-3333</p>
+            </div>
+        )
+    }
+}
+
+export default Contact
